696e17acb7709080420ab951ec68967d9d5ed91e
Tumi <happyzhangyuting@163.com>
2020-09-24 23:08:08 +0800

    feat: 优化代码

--- main.go
+++ main.go
@@ -7,12 +7,6 @@ import (
 	"red-package/rekpkg"
 )
 
-var flagV int
-
-//func init() {
-//	flag.IntVar(&flagV, "plat", 1, "输入你想抢红包的平台: 1.微信,2:飞书")
-//}
-
 func main() {
 	// 把用户传递的命令行参数解析为对应变量的值
 	fmt.Println("输入你想抢红包的平台: 1.微信,2:飞书 ")
